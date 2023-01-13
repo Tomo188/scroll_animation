a956890fb67a4da9d74dd006e9ff940c01bd7371
Tomo188 <tomislavkrtalic005@gmail.com>
2023-01-13 18:55:29 +0100

    change name file  from template.html to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
-const path=require("path")
-const htmlWebpackPlugin=require("html-webpack-plugin")
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports={
-    mode:"development",
-    entry:{bundle:path.resolve(__dirname,"index.js")},
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:"[name][contenthash].js",
-        clean:true,
-        assetModuleFilename:"[name][ext]"
-    },
-    module:{
-        rules:[
-            {
-                test:/\.scss$/,
-                use:["style-loader","css-loader","sass-loader"]
-            },{
-                test:/\.js$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:"babel-loader",
-                    options:{
-                        presets:["@babel/preset-env"]
-                    }
-                }
-            }
-        ]
-    },
-    plugins:[
-        new htmlWebpackPlugin({
-            title:"animation components",
-            filename:"index.html",
-            template:"template.html"
-        })
-    ],
-    devServer:{
-        static:{
-            directory:path.resolve(__dirname,"dist")
+module.exports = {
+  mode: "development",
+  entry: { bundle: path.resolve(__dirname, "index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        port:3000,
-        open:true,
-        hot:true,
-        compress:true,
-        historyApiFallback:true
-    }
-}
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "animation components",
+      filename: "index.html",
+      template: "index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+};
